4e2888a5af3be105e957bf531cb43fc1c5be44e5
Elise L <elli3183@gmail.com>
2025-06-04 15:36:07 +0200

    Add : cart.html
    cart page html done
    new css page (style2.css) for cart.html

--- front/js/product.js
+++ front/js/product.js
@@ -11,6 +11,8 @@ const price = document.querySelector("span");
 const buyBtn = document.querySelector(".button-buy");
 const bigDesc = document.querySelector("h2");
 const format = document.querySelector('#format');
+const titlePage = document.querySelector("title");
+console.log(titlePage)
 
 async function chargerArticles(){ //Fetch API
     try{
@@ -31,41 +33,45 @@ async function chargerArticles(){ //Fetch API
 
 const affichageArticle = (data) => {
     const product = data.find(el => el._id === productId);
-    console.log("Product : ", product)
 
-    if(product){ //vérification de la présence du produit
-        img.src = product.image; 
-        title.innerText = product.titre;
-        smallDesc.innerText = `${product.description.slice(0, 120)}...`
-        buyBtn.innerText = `Buy ${product.shorttitle}`;
-        bigDesc.innerText = product.description;
-        price.innerText = product.declinaisons[0].prix + "€"
-        let options = '';
-        product.declinaisons.forEach(el => {
-            options += `<option>${el.taille}</option>`;
-        });
-        format.innerHTML = options;
-
-        format.addEventListener('change', () => {
-            switch(format.value){
-                case "20 x 20": 
-                    price.innerText = product.declinaisons[0].prix + "€"
-                    break;
-                case "30 x 20": 
-                    price.innerText = product.declinaisons[1].prix + "€"
-                    break;
-                case "30 x 30": 
-                    price.innerText = product.declinaisons[2].prix + "€"
-                    break;
-                case "40 x 30": 
-                    price.innerText = product.declinaisons[3].prix + "€"
-                    break;
-                default: 
-                    price.innerText = product.declinaisons[4].prix + "€"
-                    break;
-            }
-        })
+    if(!product){
+        return;
     }
+
+    //Affichage de la page personnalisée en fonction du produit choisi
+    titlePage.innerText = `${product.titre} - GeniArtHub` //titre d'onglet
+    img.src = product.image; 
+    title.innerText = product.titre;
+    smallDesc.innerText = `${product.description.slice(0, 150)}...`
+    buyBtn.innerText = `Buy ${product.shorttitle}`;
+    bigDesc.innerText = product.description;
+    price.innerText = `${product.declinaisons[0].prix}€`
+
+    let options = '';
+    product.declinaisons.forEach(el => {
+        options += `<option>${el.taille}</option>`;
+    });
+    format.innerHTML = options;
+
+    format.addEventListener('change', () => { //modification du prix en temps réel selon les dimensions choisis par l'utilisateur
+        switch(format.value){
+            case "20 x 20": 
+                price.innerText = `${product.declinaisons[0].prix}€`
+                break;
+            case "30 x 20": 
+                price.innerText = `${product.declinaisons[1].prix}€`
+                break;
+            case "30 x 30": 
+                price.innerText = `${product.declinaisons[2].prix}€`
+                break;
+            case "40 x 30": 
+                price.innerText = `${product.declinaisons[3].prix}€`
+                break;
+            default: 
+                price.innerText = `${product.declinaisons[4].prix}€`
+                break;
+        }
+    })
 }
 
 chargerArticles();
